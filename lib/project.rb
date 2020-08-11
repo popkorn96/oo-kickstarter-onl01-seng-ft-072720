3de0bfbe584a9d90169c688d8326e7801c9f38c7
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-11 22:13:42 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+  attr_accessor :backers
+  attr_reader :title 
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  def add_backer(backer)
+    @backer = backer
+    @backers << backer 
+    backer.backed_projects << self
+  end
+end
